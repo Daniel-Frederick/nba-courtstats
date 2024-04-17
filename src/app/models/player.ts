cad70ed4ea7437b18a580b93621a9fce3bba9a73
Daniel Frederick <frederickd5@udayton.edu>
2024-04-17 15:35:00 -0400

    updating info of all information needed for each player

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,3 +1,14 @@
-export class Player {
-  name!: string;
+export class Player { // All current stats
+  playerid!: number; // Allows me to to look up players in the API
+  teamid!: number; // Allows me to look up teams in the API
+  fullName!: string; // First and last name
+  height!: string; // In the format: 6'5"
+  college!: string; // The college the player went throw;
+  position!: string; // The position the player has
+  NBAstartYear!: number; // The year the player started in the NBA
+  jerseyNum!: number; // The player's current jersey number
+  isActive!: boolean; // If the player is currently playing in the NBA
+  weight!: number; // How much the player weighs
+  DOB!: string; // Day Of Birth
+  country!: string; // Where the player is from
 }
