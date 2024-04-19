2b4af15c4fc5abdfb8c919f03693650c4779cbe6
Daniel Frederick <frederickd5@udayton.edu>
2024-04-18 20:26:41 -0400

    added new component to use for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './features/header/header.component';
 import { AddPlayerComponent } from './features/add-player/add-player.component';
+import { TeamTableComponent } from './features/team-table/team-table.component';
 
 const routes: Routes = [
-  { path: '', component: AddPlayerComponent }
+  { path: '', component: TeamTableComponent }
 ];
 
 @NgModule({
