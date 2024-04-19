ed22dd46f9b70ba8cfc8ca336fc8fc33707569cd
Daniel Frederick <frederickd5@udayton.edu>
2024-04-18 21:52:18 -0400

    In the progress of displaying each team name and logo

--- src/app/features/single-player/single-player.component.ts
+++ src/app/features/single-player/single-player.component.ts
@@ -10,7 +10,24 @@ export class SinglePlayerComponent implements OnInit {
   @Input() team!: Team;
 
   ngOnInit(): void {
-    console.log('single-player team: ', this.team)
+    if(this.team != undefined){
+    let content = `
+      <div>
+        <h2>{{ team.name }}<h2>
+        <img src="${this.team.logo}" alt="${this.team.name} Logo">
+      </div>
+      `
+      /*
+       1. Create div as a wrapper for the title and player table
+       2. add context(title) to the beginning of the new div
+       3. Create player table and append to the new div
+       4. append table to the already made section tag
+       */
+    }
+    else {
+      console.log('team = undefined :(')
+    }
+
  }
 
 }
