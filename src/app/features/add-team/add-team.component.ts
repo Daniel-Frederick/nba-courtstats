94b1bb5958a563c406887efe2ed26e54219ffce8
Daniel Frederick <frederickd5@udayton.edu>
2024-06-05 20:30:30 -0400

    the user can now only display one team

--- src/app/features/add-team/add-team.component.ts
+++ src/app/features/add-team/add-team.component.ts
@@ -12,16 +12,54 @@ import { PlayerTeam } from '../../models/player-team';
 export class AddTeamComponent {
   @Output() addTeamAndPlayers: EventEmitter<any> = new EventEmitter<any>();
 
-  teamName: string = 'Atlanta Hawks'; // Default User Input value
+  teams: string[] = [
+    'Atlanta Hawks',
+    'Boston Celtics',
+    'Brooklyn Nets',
+    'Charlotte Hornets',
+    'Chicago Bulls',
+    'Cleveland Cavaliers',
+    'Dallas Mavericks',
+    'Denver Nuggets',
+    'Detroit Pistons',
+    'Golden State Warriors',
+    'Houston Rockets',
+    'Indiana Pacers',
+    'Los Angeles Clippers',
+    'Los Angeles Lakers',
+    'Memphis Grizzlies',
+    'Miami Heat',
+    'Milwaukee Bucks',
+    'Minnesota Timberwolves',
+    'New Orleans Pelicans',
+    'New York Knicks',
+    'Oklahoma City Thunder',
+    'Orlando Magic',
+    'Philadelphia 76ers',
+    'Phoenix Suns',
+    'Portland Trail Blazers',
+    'Sacramento Kings',
+    'San Antonio Spurs',
+    'Toronto Raptors',
+    'Utah Jazz',
+    'Washington Wizards'
+  ]; 
+
+  teamName: string = this.teams[0]; // Default User Input value
   team!: Team; // Team Object being sent to other components
-  players!: Player[];
+  // players!: Player[];
   duplicateTeams: string[] = []; // Keeps track to already entered teams
+  isLoading: boolean = false;
 
   constructor(private playerService: PlayerService) {}
 
   async onSubmit() {
+    // Loading Started
+    this.isLoading = true;
+    this.teams = this.teams.filter(team => team !== this.teamName);
+
     // Check to see of the team is already entered
-    if (!this.duplicateTeams.includes(this.teamName)) {
+    if (!this.duplicateTeams.includes(this.teamName) && this.teamName !== '') {
       try {
         const team = await this.playerService.getTeam(this.teamName);
         this.duplicateTeams.push(team.name)
@@ -36,6 +74,12 @@ export class AddTeamComponent {
         };
         console.log('add-team teamAndPlayers: ', teamAndPlayers);
         this.addTeamAndPlayers.emit(teamAndPlayers);
+
+        // Set teamName to next team in array
+        this.teamName = this.teams[0];
+
+        // Loading stopped
+        this.isLoading = false;
       } catch (error) {
         console.error('Error:', error);
       }
